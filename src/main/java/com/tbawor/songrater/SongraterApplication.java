d575064ea2503e9cd04790445fb845543b974fa0
Tomasz Bawor <bawortomasz@gmail.com>
2017-02-21 19:42:46 +0100

    Initial commit

---
+++ src/main/java/com/tbawor/songrater/SongraterApplication.java
@@ -0,0 +1,12 @@
+package com.tbawor.songrater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongraterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongraterApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tbawor/songrater/SongraterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tbawor.songrater;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SongraterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
